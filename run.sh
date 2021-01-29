e4831e54669c73389bd009abfba781455b4721af
Metin Yaylacioglu <me.yay@mytechnet.de>
2021-01-30 00:41:18 +0100

    cleanup unessary variables

--- run.sh
+++ run.sh
@@ -1,35 +1,33 @@
 #!/bin/bash -ex
 # change mandatory
+SYNAPSE_SERVER_NAME=my.matrix.host
 SYNAPSE_HOST_PORT_HTTP=8008
-SYNAPSE_HOST_PORT_HTTPS=8009
-SYNAPSE_HOST_PORT_FEDERATION=8448
 SYNAPSE_UID=991
 SYNAPSE_GID=991
-SYNAPSE_SERVER_NAME=my.matrix.host
 
 SYNAPSE_VOLUME_HOST_PATH=/volume2/docker/synapse/app/data
-
 POSTGRES_VOLUME_HOST_PATH=/volume2/docker/synapse/db/data
 
 # change optional
-SYNAPSE_IMAGE=matrixdotorg/synapse:latest
 SYNAPSE_MAX_UPLOAD_SIDE=64M
 SYNAPSE_ENABLE_REGISTRATION=true
 
+POSTGRES_HOST=db
+POSTGRES_DB=synapse
+POSTGRES_USER=synapse
+POSTGRES_PASSWORD=synapse
+
+# change if you know what you're doing
+SYNAPSE_IMAGE=matrixdotorg/synapse:latest
 SYNAPSE_REPORT_STATS=no
+SYNAPSE_TZ=Europe/Berlin
+SYNAPSE_NO_TLS=true
 SYNAPSE_DATA_DIR=/data
 SYNAPSE_CONFIG_DIR=/data
 SYNAPSE_CONFIG_PATH=${SYNAPSE_CONFIG_DIR}/homeserver.yaml
-SYNAPSE_TZ=Europe/Berlin
-SYNAPSE_NO_TLS=true
 SYNAPSE_WORKER=synapse.app.homeserver
 
-POSTGRES_USER=synapse
-POSTGRES_PASSWORD=synapse
-POSTGRES_HOST=db
-POSTGRES_DB=synapse
 POSTGRES_IMAGE=postgres:13.1-alpine
-
 POSTGRES_INITDB_ARGS="--encoding=UTF-8 --lc-collate=C --lc-ctype=C"
 
 # project name, will be prefix to *_SERVICE_NAME in container names
@@ -41,7 +39,7 @@ create_volume_host_path(){
         if [ ! -e "${SYNAPSE_VOLUME_HOST_PATH}" ];then
             mkdir -p "${path}"
         fi
-    done 
+    done
 }
 
 
@@ -65,6 +63,7 @@ create_homeserver_yaml(){
             ${SYNAPSE_IMAGE} generate
     fi
 }
+
 render_homeserver_yaml(){
     opwd="$PWD"
     cd $( dirname "$0" )
@@ -83,6 +82,7 @@ render_homeserver_yaml(){
 chown_synapse_volume_host_path(){
     chown ${SYNAPSE_UID}:${SYNAPSE_GID} -R "${SYNAPSE_VOLUME_HOST_PATH}"
 }
+
 render_compose_file_and_execute(){
     # render variables into template file to create final docker-compose.yml
     opwd="$PWD"
@@ -90,6 +90,7 @@ render_compose_file_and_execute(){
     eval "echo \"$(<docker-compose.template)\"" | docker-compose --project-name "${DOCKER_COMPOSE_PROJECT}" --file  - $@ 
     cd "${opwd}"
 }
+
 create_nginx_reverse_proxy_config(){
     # identify letsencryp certificate path
     if [ -e /etc/nginx/appd.d/server.synapse.conf ];then
@@ -107,6 +108,7 @@ create_nginx_reverse_proxy_config(){
         #nginx -s reload
     fi
 }
+
 if [ -z "$1" ];then
     echo "provide param: prepare, fix or any docker-compose parameter"
     exit 1
