40e60f93c146bc707e861f57f7c92071cd7eec39
Metin Yaylacioglu <me.yay@mytechnet.de>
2021-01-29 20:07:50 +0100

    inital commit

---
+++ README.md
@@ -0,0 +1,10 @@
+#  create_networks: implizit über docker-compose, nur relevant bei pgadmin
+#  create data folders -> done
+#  setup postgres container -> done, part of compose
+#  setup pgadmin contaienr -> won't do
+#  maintaine reverse proxy setting for container -> TODO
+#  create synapse container -> done
+#  create homeserver.yml  -> done
+#  create domain specific log file ->  done
+#  render config in homeserver.yml -> done
+#  fix file permissions -> partialy done
---
+++ run.sh
@@ -0,0 +1,114 @@
+#!/bin/bash -ex
+# change mandatory
+SYNAPSE_HOST_PORT_HTTP=8008
+SYNAPSE_HOST_PORT_HTTPS=8009
+SYNAPSE_HOST_PORT_FEDERATION=8448
+SYNAPSE_UID=991
+SYNAPSE_GID=991
+SYNAPSE_SERVER_NAME=my.matrix.host
+
+SYNAPSE_VOLUME_HOST_PATH=/volume2/docker/synapse/app/data
+
+POSTGRES_VOLUME_HOST_PATH=/volume2/docker/synapse/db/data
+
+# change optional
+SYNAPSE_IMAGE=matrixdotorg/synapse:latest
+SYNAPSE_MAX_UPLOAD_SIDE=64M
+SYNAPSE_ENABLE_REGISTRATION=true
+
+SYNAPSE_REPORT_STATS=no
+SYNAPSE_DATA_DIR=/data
+SYNAPSE_CONFIG_DIR=/data
+SYNAPSE_CONFIG_PATH=${SYNAPSE_CONFIG_DIR}/homeserver.yaml
+SYNAPSE_TZ=Europe/Berlin
+SYNAPSE_NO_TLS=true
+SYNAPSE_WORKER=synapse.app.homeserver
+
+POSTGRES_USER=synapse
+POSTGRES_PASSWORD=synapse
+POSTGRES_HOST=db
+POSTGRES_DB=synapse
+POSTGRES_IMAGE=postgres:13.1-alpine
+
+POSTGRES_INITDB_ARGS="--encoding=UTF-8 --lc-collate=C --lc-ctype=C"
+
+# project name, will be prefix to *_SERVICE_NAME in container names
+DOCKER_COMPOSE_PROJECT=matrix
+
+create_volume_host_path(){
+    
+    for path in "${SYNAPSE_VOLUME_HOST_PATH}" "${POSTGRES_VOLUME_HOST_PATH}"; do
+        if [ ! -e "${SYNAPSE_VOLUME_HOST_PATH}" ];then
+            mkdir -p "${path}"
+        fi
+    done 
+}
+
+
+create_log_config(){
+    if [ ! -e "${SYNAPSE_VOLUME_HOST_PATH}/${SYNAPSE_SERVER_NAME}.log.config" ];then
+        cp $( dirname "$0" )/log.template "${SYNAPSE_VOLUME_HOST_PATH}/${SYNAPSE_SERVER_NAME}.log.config"
+    fi
+}
+
+create_homeserver_yaml(){
+    if [ ! -e "${SYNAPSE_VOLUME_HOST_PATH}/homeserver.yaml" ];then
+        docker run -it --rm \
+            -e SYNAPSE_SERVER_NAME=${SYNAPSE_SERVER_NAME} \
+            -e SYNAPSE_REPORT_STATS=${SYNAPSE_REPORT_STATS} \
+            -e SYNAPSE_CONFIG_DIR=${SYNAPSE_CONFIG_DIR} \
+            -e SYNAPSE_CONFIG_PATH=${SYNAPSE_CONFIG_PATH} \
+            -e SYNAPSE_DATA_DIR=${SYNAPSE_DATA_DIR} \
+            -e UID=${SYNAPSE_UID} \
+            -e GID=${SYNAPSE_GID} \
+            -v "${SYNAPSE_VOLUME_HOST_PATH}:${SYNAPSE_CONFIG_DIR}:rw" \
+            ${SYNAPSE_IMAGE} generate
+    fi
+}
+render_homeserver_yaml(){
+    opwd="$PWD"
+    cd $( dirname "$0" )
+    if [ ! -e "${SYNAPSE_VOLUME_HOST_PATH}/homeserver.yaml.bak" ]; then
+        mv "${SYNAPSE_VOLUME_HOST_PATH}/homeserver.yaml" "${SYNAPSE_VOLUME_HOST_PATH}/homeserver.yaml.bak"
+    fi 
+    homeserver=$( grep -vE '(^#|\s#|^$)' "${SYNAPSE_VOLUME_HOST_PATH}/homeserver.yaml.bak")
+    SYNAPSE_MACAROON_SECRET_KEY=$(grep --perl-regexp --only-matching '(?<=^macaroon_secret_key: ").*(?="$)' <<< "${homeserver}" )
+    SYNAPE_REGISTRATION_SHARED_SECRET=$(grep --perl-regexp --only-matching '(?<=^registration_shared_secret: ").*(?="$)' <<< "${homeserver}" )
+    SYNAPSE_FROM_SECRET=$(grep --perl-regexp --only-matching '(?<=^form_secret: ").*(?="$)' <<< "${homeserver}" )
+    eval "echo \"$(<homeserver.yaml.template)\"" > "${SYNAPSE_VOLUME_HOST_PATH}/homeserver.yaml"
+
+    cd "${opwd}"
+}
+
+chown_synapse_volume_host_path(){
+    chown ${SYNAPSE_UID}:${SYNAPSE_GID} -R "${SYNAPSE_VOLUME_HOST_PATH}"
+}
+render_compose_file_and_execute(){
+    # render variables into template file to create final docker-compose.yml
+    opwd="$PWD"
+    cd $( dirname "$0" )
+    eval "echo \"$(<docker-compose.template)\"" | docker-compose --project-name "${DOCKER_COMPOSE_PROJECT}" --file  - $@ 
+    cd "${opwd}"
+}
+
+if [ -z "$1" ];then
+    echo "provide param: prepare, fix or any docker-compose parameter"
+    exit 1
+fi
+set -u
+case "$1" in
+
+    prepare)    create_volume_host_path
+                create_log_config
+                create_homeserver_yaml
+                render_homeserver_yaml
+                chown_synapse_volume_host_path
+                ;;
+
+    fixperms)   chown_synapse_volume_host_path
+                ;;
+
+    *)          render_compose_file_and_execute
+                ;;
+
+esac
